91a9f8f311933a8798abda287a0d65345adc9ebd
Chou Cheng-Yao <anti.ning@gmail.com>
2017-03-14 22:04:06 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
+//constants
 var FLOOR_HEIGHT = 50;
 var lineWIDTH = 5;
 var WIDTH = 1000;
 var HEIGHT = 750;
+var MAP_WIDTH = 2000;
+var Present_mapLeft = 0;
 
 class Root{
-	constructor(al, au){
-		this.absLeft = al;
+	constructor(ml, au){
+		this.mapLeft = ml;
 		this.absUp = au;
 		this.width;
 		this.height;
@@ -13,17 +16,17 @@ class Root{
 }
 
 class Barrier extends Root{
-	constructor(al,au,w,h,I){
-		super(al,au);
+	constructor(ml,au,w,h,I){
+		super(ml,au);
 		this.width = w;
 		this.height = h;
-		this.isImmortal = I;
+		this.isImmortml = I;
 	}
 }
 
 class Player extends Root{
-	constructor(al,au,n){
-		super(al,au);
+	constructor(ml,au,n){
+		super(ml,au);
 		this.width = 50;
 		this.height = 80;
 		this.name = n;
@@ -35,7 +38,7 @@ class Player extends Root{
 		panel.fillStyle = "#90c2f4";
 		panel.fillRect(0, 0,40,15);
 		panel.fillStyle = "#000000";
-		panel.font = "15px Arial";
+		panel.font = "15px Ariml";
 		panel.fillText(this.name, 3 ,13);
 		//hp
 		panel.fillStyle = "#f1043b";
@@ -43,33 +46,43 @@ class Player extends Root{
 		panel.fillStyle = "#04be11";
 		panel.fillRect(40, 0,this.hp,15);
 		panel.fillStyle = "#044fbe";
-		panel.font = "15px Arial";
+		panel.font = "15px Ariml";
 		panel.fillText("HP "+this.hp,70 ,13);
 		//player
 		panel.fillStyle = "#0C8763";
-		panel.fillRect(this.absLeft, HEIGHT-this.absUp-FLOOR_HEIGHT,50,60);
+		panel.fillRect(this.mapLeft - Present_mapLeft, HEIGHT-this.absUp-FLOOR_HEIGHT,50,60);
 		panel.fillStyle = "#C4C400";
-		panel.fillRect(this.absLeft+20,HEIGHT-this.absUp+30-FLOOR_HEIGHT,10,25);
-		if((this.absLeft/10)%6>=3){
+		panel.fillRect(this.mapLeft+20 - Present_mapLeft,HEIGHT-this.absUp+30-FLOOR_HEIGHT,10,25);
+		if((this.mapLeft/10)%6>=3){
 			panel.fillStyle = "#e90c0c";
-			panel.fillRect(this.absLeft+15,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
+			panel.fillRect(this.mapLeft+15 -Present_mapLeft,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
 			panel.fillStyle = "#0C8763";
-			panel.fillRect(this.absLeft+20,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
+			panel.fillRect(this.mapLeft+20 -Present_mapLeft,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
 		}
 		else{
 			panel.fillStyle = "#e90c0c";
-			panel.fillRect(this.absLeft+20,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
+			panel.fillRect(this.mapLeft+20 -Present_mapLeft,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
 			panel.fillStyle = "#0C8763";
-			panel.fillRect(this.absLeft+15,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
+			panel.fillRect(this.mapLeft+15 -Present_mapLeft,HEIGHT-this.absUp+60-FLOOR_HEIGHT,15,19);
+		}
+	}
+
+	xmoving(distance){
+		if(this.mapLeft + this.width >= WIDTH/2 && MAP_WIDTH - this.mapLeft >= WIDTH/2 && Present_mapLeft + distance >=0 && Present_mapLeft +distance +WIDTH <=MAP_WIDTH){
+			this.mapLeft += distance;
+			Present_mapLeft += distance;
+		}
+		else{
+			this.mapLeft += distance;
 		}
 	}
 
 	forward(){
-		this.absLeft += 5;
+		this.xmoving(5);
 	}
 
 	backward(){
-		this.absLeft -= 5;
+		this.xmoving(-5);
 	}
 
 	upward(){
@@ -79,47 +92,47 @@ class Player extends Root{
 }
 
 class Box extends Barrier{
-	constructor(al,au,w,h,s,px){
-		super(al,au,w,h,0);
+	constructor(ml,au,w,h,s,px){
+		super(ml,au,w,h,0);
 		this.symbol = s;
 		this.size = px;
 	}
 	draw(panel){
 		panel.fillStyle = "#000000";
-		panel.fillRect(this.absLeft,HEIGHT-this.absUp,this.width,this.height);
+		panel.fillRect(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp,this.width,this.height);
 		panel.fillStyle = "#FFFFFF";
-		panel.font = this.size +"px Arial";
-		panel.fillText(this.symbol, this.absLeft, HEIGHT - this.absUp +this.height-5);
+		panel.font = this.size +"px Ariml";
+		panel.fillText(this.symbol, this.mapLeft -Present_mapLeft, HEIGHT - this.absUp +this.height-5);
 		panel.strokeStyle = "brown";
 		panel.beginPath();
-		panel.moveTo(this.absLeft,HEIGHT-this.absUp);
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp);
+		panel.moveTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp);
 		panel.stroke();
 		panel.beginPath();
-		panel.moveTo(this.absLeft,HEIGHT-this.absUp);
-		panel.lineTo(this.absLeft,HEIGHT-this.absUp+this.height);
+		panel.moveTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp);
+		panel.lineTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp+this.height);
 		panel.stroke();
-		panel.lineTo(this.absLeft,HEIGHT-this.absUp+this.height);
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp+this.height);
+		panel.lineTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp+this.height);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp+this.height);
 		panel.stroke();
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp);
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp+this.height);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp+this.height);
 		panel.stroke();
 	}
 }
 
 class Brick extends Barrier{
-	constructor(al,au,w,h){
-		super(al,au,w,h,1);
+	constructor(ml,au,w,h){
+		super(ml,au,w,h,1);
 	}
 	draw(panel){
 		panel.fillStyle = "#000000";
-		panel.fillRect(this.absLeft,HEIGHT-this.absUp,this.width,this.height);
+		panel.fillRect(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp,this.width,this.height);
 		for(var i = 0;i<this.absUp/10;i++){
 			panel.beginPath();
 			panel.strokeStyle = "brown";
-			panel.moveTo(this.absLeft,HEIGHT-this.absUp+i*10);
-			panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp+i*10);
+			panel.moveTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp+i*10);
+			panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp+i*10);
 			panel.stroke();
 			for(var j = 0;j<this.width/20;j++){
 				var offset = 0;
@@ -127,25 +140,25 @@ class Brick extends Barrier{
 				offset = 10;
 				panel.beginPath();
 				panel.strokeStyle = "brown";
-				panel.moveTo(this.absLeft+j*20+offset,HEIGHT-this.absUp+i*10);
-				panel.lineTo(this.absLeft+j*20+offset,HEIGHT-this.absUp+(i+1)*10);
+				panel.moveTo(this.mapLeft -Present_mapLeft+j*20+offset,HEIGHT-this.absUp+i*10);
+				panel.lineTo(this.mapLeft -Present_mapLeft+j*20+offset,HEIGHT-this.absUp+(i+1)*10);
 				panel.stroke();
 			}
 		}
 		panel.strokeStyle = "brown";
 		panel.beginPath();
-		panel.moveTo(this.absLeft,HEIGHT-this.absUp);
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp);
+		panel.moveTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp);
 		panel.stroke();
 		panel.beginPath();
-		panel.moveTo(this.absLeft,HEIGHT-this.absUp);
-		panel.lineTo(this.absLeft,HEIGHT-this.absUp+this.height);
+		panel.moveTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp);
+		panel.lineTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp+this.height);
 		panel.stroke();
-		panel.lineTo(this.absLeft,HEIGHT-this.absUp+this.height);
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp+this.height);
+		panel.lineTo(this.mapLeft -Present_mapLeft,HEIGHT-this.absUp+this.height);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp+this.height);
 		panel.stroke();
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp);
-		panel.lineTo(this.absLeft+this.width,HEIGHT-this.absUp+this.height);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp);
+		panel.lineTo(this.mapLeft -Present_mapLeft+this.width,HEIGHT-this.absUp+this.height);
 		panel.stroke();
 	}
 }
